5834b73b5dd6779aac763ac6393f23431c9a0b09
Hamilton Chapman <hamchapman@gmail.com>
2018-01-16 10:06:09 +0000

    Add update_role_permissions example

---
+++ examples/update_role_permissions.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/chatkit'
+
+chatkit = Chatkit::Client.new({
+  instance_locator: "v1:us1:some-instance-id",
+  key: "the-id-bit:the-secret-bit"
+})
+
+p chatkit.update_role_permissions("default", "global", ["cursors:read:get"], ["cursors:read:set"])
