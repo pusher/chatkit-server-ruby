b1f98a58a8c0a2337f79b1aa7b255c00bf1956d0
Hamilton Chapman <hamchapman@gmail.com>
2018-04-20 16:10:10 +0100

    Add authenticate_with_request example

---
+++ examples/authenticate_with_request.rb
@@ -0,0 +1,28 @@
+require_relative '../lib/chatkit'
+
+chatkit = Chatkit::Client.new({
+  instance_locator: 'v1:us1:some-instance-id',
+  key: 'the-id-bit:the-secret-bit'
+})
+
+# FakeRequest and FakeRequestBody are used here to mimic Rack::Request
+
+class FakeRequest
+  def initialize
+  end
+
+  def body
+    FakeRequestBody.new
+  end
+end
+
+class FakeRequestBody
+  def initialize
+  end
+
+  def read
+    'grant_type=client_credentials'
+  end
+end
+
+p chatkit.authenticate_with_request(FakeRequest.new, { user_id: 'testymctest' })
