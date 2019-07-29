727fc5d144d57c1f589835b8174c0948ce9e74b9
Jonathan Lloyd <jonathan@thisisjonathan.com>
2019-07-29 11:36:00 +0100

    Accept nil in update_room

--- lib/chatkit/client.rb
+++ lib/chatkit/client.rb
@@ -262,7 +262,7 @@ module Chatkit
       payload = {}
       payload[:name] = options[:name] unless options[:name].nil?
       payload[:private] = options[:private] unless options[:private].nil?
-      body[:push_notification_title_override] = options[:push_notification_title_override] unless options[:push_notification_title_override].nil?
+      body[:push_notification_title_override] = options[:push_notification_title_override] if options.key?(:push_notification_title_override) # We want to accept nil
       payload[:custom_data] = options[:custom_data] unless options[:custom_data].nil?
 
       api_request({
